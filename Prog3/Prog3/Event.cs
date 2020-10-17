cd9b7a64493ea89aeea575489ab7dcc6dcf175ab
David Greiff <greiffd@uwplatt.edu>
2020-10-16 22:59:02 -0500

    Added Events.xml because it was ignored

---
+++ Prog3/Prog3/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prog3
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        //public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
